d76d07ed5c8778819942f9bfb8743eaf7f83a8ab
Xavier Dutoit <xavier@tttp.eu>
2012-10-27 13:17:42 +0200

    Companie have multiple employees

--- contact.js
+++ contact.js
@@ -26,49 +26,52 @@ api.get("Contact",0,function(data) {
 
 
 var generateContacts = function (total) {
+  //var locale = ['en-us','en-gb','de','nl','pt-br'];
+  var locale = ['en-us','en-gb','de','nl'];
+  var length = locale.length;
+  var rateNewEmployer = 0.2; //how often to create a new employer or keep the existing one
+  var employer = Charlatan.Company.name();
+  for (var i=0;i< total;i++) { 
+  //for (var i=0;i<1;i++) { 
+    var lang = locale[Math.floor(Math.random()*locale.length)];
+    Charlatan.setLocale(lang);
 
-//var locale = ['en-us','en-gb','de','nl','pt-br'];
-var locale = ['en-us','en-gb','de','nl'];
-var length = locale.length;
-for (var i=0;i< total;i++) { 
-//for (var i=0;i<1;i++) { 
-  var lang = locale[Math.floor(Math.random()*locale.length)];
-  Charlatan.setLocale(lang);
-
-    //'debug':1,
-
-  var param = {
-    'contact_type' : 'Individual',
-    'preferred_language': lang,
-    'source':'imaginary friend',
-    'first_name'    : Charlatan.Name.firstName(),
-    'last_name'    : Charlatan.Name.lastName(),
-    'job_title': Charlatan.Name.title(),
-    'email'   : Charlatan.Internet.email(),  // glover_ii@voluptas.name
-    'current_employer' : Charlatan.Company.name(),    // Wilkinson LLC
-    'api.phone.create': {
-      'phone':Charlatan.PhoneNumber.phoneNumber(),
-    },
-    'api.address.create': {
-      'location_type_id':1,
-      'street_address':Charlatan.Address.streetAddress(),
-      'postal_code': Charlatan.Address.postcode(),
-      'city':Charlatan.Address.city(),
-      'state':Charlatan.Address.state(),
+    if (Math.random() <= rateNewEmployer) {
+      employer = Charlatan.Company.name();
     }
+
+      //'debug':1,
+    var param = {
+      'contact_type' : 'Individual',
+      'preferred_language': lang,
+      'source':'imaginary friend',
+      'first_name'    : Charlatan.Name.firstName(),
+      'last_name'    : Charlatan.Name.lastName(),
+      'job_title': Charlatan.Name.title(),
+      'email'   : Charlatan.Internet.email(),
+      'current_employer' : employer,
+      'api.phone.create': {
+        'phone':Charlatan.PhoneNumber.phoneNumber(),
+      },
+      'api.address.create': {
+        'location_type_id':1,
+        'street_address':Charlatan.Address.streetAddress(),
+        'postal_code': Charlatan.Address.postcode(),
+        'city':Charlatan.Address.city(),
+        'state':Charlatan.Address.state(),
+      }
+    }
+
+    Charlatan.setLocale('en-us');//or the language of the install
+    param['api.address.create'].country = Charlatan.Address.country();
+    
+    api.create("contact",param,function(data) {
+      if (data.is_error) {
+        console.log(data);
+      } else {
+        console.log ("created "+data.id+ " " + data.values[0].sort_name);
+      }
+    });
   }
-
-  Charlatan.setLocale('en-us');//or the language of the install
-  param['api.address.create'].country = Charlatan.Address.country();
-  
-  api.create("contact",param,function(data) {
-    if (data.is_error) {
-      console.log(data);
-    } else {
-      console.log ("created "+data.id+ " " + data.values[0].sort_name);
-    }
-  });
-//  console.log (param);
-}
 }
 
